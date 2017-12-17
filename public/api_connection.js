655fa5a57a5de29d54ac00fb3e011bd4560ac1c9
Marta <martadabrowka@gmail.com>
2017-12-17 13:51:11 +0000

    linked new api to my project

---
+++ public/api_connection.js
@@ -0,0 +1,18 @@
+var app = function(){
+
+  var request = require('superagent');
+
+  var clientID = 'e15c000c39a367b25832',
+      clientSecret = 'b229b34c4b89a1b10a2c26afe6022d7e',
+      apiUrl = 'https://api.artsy.net/api/tokens/xapp_token',
+      xappToken;
+
+  request
+    .post(apiUrl)
+    .send({ client_id: clientID, client_secret: clientSecret })
+    .end(function(res) {
+      xappToken = res.body.token;
+    });
+
+
+}
--- public/index.js
+++ public/index.js
@@ -1,5 +1,28 @@
 var app = function(){
 
+  const url = 'https://newsapi.org/v2/top-headlines?' +
+          'sources=bbc-news&' +
+          'apiKey=afd0f79d78284450b15a185df3c14f55'
+  makeRequest(url, requestComplete)
 }
+
+const requestComplete = function() {
+  if(this.status !== 200) {
+    return;
+  }
+
+  const jsonString = this.responseText;
+  const newsListConvertedFromJson = JSON.parse(jsonString);
+
+console.log(newsListConvertedFromJson);
+}
+
+const makeRequest = function(url, callback) {
+  const request = new XMLHttpRequest();
+  request.open('GET', url);
+  request.addEventListener('load', callback);
+  request.send();
+}
+
 
 document.addEventListener('DOMContentLoaded', app);
